b6c3f15908b7787e72317f5ad41a91a67f151692
Pierre <pierre.geronimus@laposte.net>
2022-11-10 15:24:55 +0100

    Ajout squelette main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+/**
+ * \file main.c
+ * \brief Programme principal du projet Pentomino
+ * \author Pierre Géronimus et Baptiste Ruault
+ * \version 1.0
+ * \date 10 novembre 2022
+ */
+
+/* include */
+
+
+/**
+ * \brief Programme principal qui implémente la boucle du jeu
+ */
+int main(int argc, char *args[]){
+  
+  //Données
+  
+  //Initialisation du jeu.
+  /* code ... */
+  
+  // Boucle de jeu
+  while(0 /* condition d'arrêt */){
+    
+    //Gestion des évènements
+
+    //Mise à jour des données liées au monde
+    
+    //Rafraîchissement de l'écran
+    
+    //Pause de 10ms pour contrôler la vitesse de rafraîchissement
+    pause(10);
+  }
+  
+  //Nettoyage final
+  
+  return 0;
+}
