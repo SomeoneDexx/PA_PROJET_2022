ae10759baf7423e2d3ead219f1d747d678478ba3
Baptiste Ruault <baptiste.ruault6@etu.univ-lorraine.fr>
2022-11-16 11:59:24 +0100

    Ajout des documentation de grille.h et blocs.h

--- blocs.h
+++ blocs.h
@@ -1,24 +1,94 @@
 #ifndef BLOCS_H
 #define BLOCS_H
 
-enum couleur { ROUGE, VERT_F, VERT_C, ROSE, BRUN, JAUNE, BLEU_F, BLEU_C, VIOLET, MAGENTA, CYAN, ORANGE, BLANC, GRIS_C, GRIS_F, NOIR};
-
-typedef struct Bloc bloc_t;
-struct Bloc
-{
-    int l; // Largeur du bloc
-    int h; // Hauteur du bloc
-    char** tab; // Tableau de taille l*h
-    enum couleur coul; // Couleur du bloc
+/**
+ * \brief Les couleurs possibles des blocs
+*/
+enum couleur { 
+    ROUGE,
+    VERT_F,
+    VERT_C,
+    ROSE, 
+    BRUN, 
+    JAUNE, 
+    BLEU_F, 
+    BLEU_C, 
+    VIOLET, 
+    MAGENTA, 
+    CYAN, 
+    ORANGE, 
+    BLANC, 
+    GRIS_C, 
+    GRIS_F, 
+    NOIR
 };
 
-char** allouer_bloc(int x, int y, enum couleur coul);
-void desallouer_bloc(char** tab, int l);
+/**
+ * \brief Représentation d'un bloc 
+*/
+typedef struct Bloc bloc_t;
+struct Bloc {
+    int lignes; 
+    int colonnes;
+    char** tab; 
+    enum couleur coul; 
+};
+
+/**
+ * \brief Fonction qui allouer un bloc en memoire
+ * \param x L'abscisse du bloc
+ * \param y L'ordonnee du bloc
+ * \param coul La couleur du bloc
+*/
+bloc_t allouer_bloc(int x, int y, enum couleur coul);
+
+/**
+ * \brief Fonction qui desalloue un bloc enn memoire
+ * \param bloc Le bloc a desallouer
+ * \param l Le nombre de ligne du bloc, sa hauteur
+*/
+void desallouer_bloc(bloc_t bloc);
+
+/**
+ * \brief Fonction qui affiche un bloc
+ * \param bloc Le bloc a afficher
+*/
 void afficher_bloc(bloc_t bloc);
-void taille_bloc(const char* nom_fichier, bloc_t* bloc);
-char** lire_bloc(const char* nom_fichier, int ind);
-void modif_bloc(int x, int y, enum couleur coul);
+
+/**
+ * \brief Fonction qui allouer un bloc en memoire
+ * \param bloc Le bloc dont on veut les dimensions
+ * \param nbLig Parametre qui servira a changer la valeur de la variable rentree
+ * \param nbCol Parametre qui servira a changer la valeur de la variable rentree
+*/
+void taille_bloc(bloc_t bloc, int* nbLig, int* nbCol);
+
+/**
+ * \brief Fonction qui recupere un bloc a l'indice en parametre dansun fichier precise
+ * \param nom_fichier Le fichier dans lequel recuperer le bloc
+ * \param ind L'indice du bloc a recuperer
+*/
+bloc_t lire_bloc(const char* nom_fichier, int ind); 
+
+/**
+ * \brief Fonction qui change les valeurs attributs du bloc 
+ * \param bloc Le bloc a modifier
+ * \param lignes Le nombre de lignes voulu
+ * \param colonnes Le nombre de colonnes voulu
+ * \param coul La couleur voulue
+*/
+void modif_bloc(bloc_t bloc, int lignes, int colonnes, enum couleur coul);
+
+/**
+ * \brief Fonction qui fait tourner un bloc dans le sens horaire
+ * \param bloc Le bloc a faire tourner
+*/
 void rotation_sh(bloc_t bloc);
+
+/**
+ * \brief Fonction qui fait tourner un bloc dans le sens anti-horaire
+ * \param bloc Le bloc a faire tourner
+*/
 void rotation_sah(bloc_t bloc);
 
 #endif
--- grille.h
+++ grille.h
@@ -10,21 +10,21 @@
 typedef struct Grille grille_t;
 struct Grille {
     char** elements;
-    int hauteur;
-    int largeur;
+    int lignes;
+    int colonnes;
 };
 
 /**
  * \brief Fonction qui alloue la grille en memoire
- * \param largeur Largeur de la grille de jeu, en fonction du nombre de colonne du fichier lu
- * \param hauteur Hauteur de la grille de jeu, en fonction du nombre de ligne du fichier lu
+ * \param colonnes colonnes de la grille de jeu, en fonction du nombre de colonne du fichier lu
+ * \param lignes lignes de la grille de jeu, en fonction du nombre de ligne du fichier lu
 */
-grille_t allouer_grille(int largeur, int hauteur);
+grille_t allouer_grille(int colonnes, int lignes);
 
 /**
  * \brief Fonction qui detruit la grille en memoire
  * \param grille La grille a detruire
- * \param lignes La hauteur de la grille a detruire
+ * \param lignes La lignes de la grille a detruire
 */
 void desallouer_grille(grille_t grille, int lignes);
 
